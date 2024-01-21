440b4189ef934b04ee476a13f3445c088a1b36a0
oto48 <otchokhonelidze4@gmail.com>
2024-01-21 17:58:26 +0400

    add styles

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -3,40 +3,61 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <title>Laravel</title>
     </head>
     <body>
-        <form action="/" method="post">
-            @csrf
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required>
-
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-
-            <label for="message">Message:</label>
-            <textarea id="message" name="message" rows="4" required></textarea>
-
-            <button type="submit">Submit</button>
-        </form>
-
-        <div>
-            <h2>Messages:</h2>
-            @if($data)
-                <ul>
-                    @foreach($data as $item)
-                        <li>
-                            <strong>Name:</strong> {{ $item['name'] }}<br>
-                            <strong>Email:</strong> {{ $item['email'] }}<br>
-                            <strong>Message:</strong> {{ $item['message'] }}<br>
-                            <strong>Created at:</strong> {{ $item['created_at'] }}<br><br>
-                        </li>
-                    @endforeach
-                </ul>
-            @else
-                <p>No messages.</p>
-            @endif
+        <div class="container">
+            <form action="/" method="post">
+                @csrf
+                <div class="row">
+                    <div class="col-25">
+                        <label for="name">Name</label>
+                    </div>
+                    <div class="col-75">
+                        <input type="text" id="name" name="name" placeholder="Your name..">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-25">
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="col-75">
+                        <input type="email" id="email" name="email" placeholder="Your email..">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-25">
+                        <label for="message">Message</label>
+                    </div>
+                    <div class="col-75">
+                        <textarea id="message" name="message" placeholder="Write something.."></textarea>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <input type="submit" value="Submit">
+                </div>
+            </form>
         </div>
+
+        <table class="data">
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Message</th>
+                <th>Created at</th>
+            </tr>
+            @if($data)
+                @foreach($data as $item)
+                    <tr>
+                        <td>{{ $item['name'] }}</td>
+                        <td>{{ $item['email'] }}</td>
+                        <td>{{ $item['message'] }}</td>
+                        <td>{{ $item['created_at'] }}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </table>
     </body>
 </html>
